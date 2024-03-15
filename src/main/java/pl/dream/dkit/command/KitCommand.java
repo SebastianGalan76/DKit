b8f11278dbdb9993e7426a06b3b2c93721bfb1ce
Sebastian Gałan <sebastian.galan7625@gmail.com>
2024-03-15 21:26:47 +0100

    Added a give subcommand

--- src/main/java/pl/dream/dkit/command/KitCommand.java
+++ src/main/java/pl/dream/dkit/command/KitCommand.java
@@ -1,5 +1,6 @@
 package pl.dream.dkit.command;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,15 +11,21 @@ import pl.dream.dkit.Locale;
 import pl.dream.dkit.data.kit.IKit;
 import pl.dream.dkit.data.kit.Kit;
 import pl.dream.dkit.data.LocalPlayer;
+import pl.dream.dreamlib.Color;
 import pl.dream.dreamlib.Message;
 
 public class KitCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
         if(!(sender instanceof Player)){
-            if(args.length>0 && args[0].equalsIgnoreCase("reload")){
-                DKit.getPlugin().reloadPlugin();
-                Message.sendMessage(sender, Locale.RELOAD.toString());
+            if(args.length>0){
+                if(args[1].equalsIgnoreCase("reload")){
+                    DKit.getPlugin().reloadPlugin();
+                    Message.sendMessage(sender, Locale.RELOAD.toString());
+                }
+                else if(args[1].equalsIgnoreCase("give")){
+                    give(sender, args);
+                }
             }
 
             return true;
@@ -34,12 +41,9 @@ public class KitCommand implements CommandExecutor {
         }
         else if(args.length==1){
             if(args[0].equalsIgnoreCase("reload")){
-                if(sender.hasPermission("dkit.kit.reload")){
+                if(checkPermission(sender, "dkit.kit.reload")){
                     DKit.getPlugin().reloadPlugin();
                     Message.sendMessage(sender, Locale.RELOAD.toString());
-                }
-                else{
-                    Message.sendMessage(sender, Locale.NO_PERMISSIONS.toString());
                 }
             }
             else{
@@ -54,7 +58,59 @@ public class KitCommand implements CommandExecutor {
             }
         }
         else{
+            if(args[0].equalsIgnoreCase("give")){
+                give(sender, args);
+            }
+        }
 
+        return true;
+    }
+
+    private void give(CommandSender sender, String[] args){
+        if(checkPermission(sender, "dkit.kit.give")){
+            if(args.length==3){
+                if(args[1].equalsIgnoreCase("*")){
+                    IKit kit = DKit.getPlugin().kits.get(args[2]);
+                    if(kit==null){
+                        Message.sendMessage(sender, Locale.NO_KIT.toString());
+                        return;
+                    }
+
+                    for(Player player:Bukkit.getOnlinePlayers()){
+                        kit.giveKit(player);
+                    }
+                }
+                else{
+                    Player player = Bukkit.getPlayer(args[1]);
+                    IKit kit = DKit.getPlugin().kits.get(args[2]);
+
+                    if(player==null){
+                        Message.sendMessage(sender, Locale.NO_PLAYER.toString());
+                        return;
+                    }
+                    if(kit==null){
+                        Message.sendMessage(sender, Locale.NO_KIT.toString());
+                        return;
+                    }
+
+                    kit.giveKit(player);
+                    if(sender instanceof Player){
+                        Message.sendMessage(sender, Locale.GIVE_KIT_TO_PLAYER.toString()
+                                .replace("{PLAYER}", player.getName())
+                                .replace("{KIT}", args[2]));
+                    }
+                }
+            }
+            else{
+                Color.sendMessage(sender, "&7Poprawne użycie: &c/kit give [nick/*] [kit]");
+            }
+        }
+    }
+
+    private boolean checkPermission(CommandSender sender, String permission){
+        if(!sender.hasPermission(permission)){
+            Message.sendMessage(sender, Locale.NO_PERMISSIONS.toString());
+            return false;
         }
 
         return true;
--- src/main/java/pl/dream/dkit/data/kit/IKit.java
+++ src/main/java/pl/dream/dkit/data/kit/IKit.java
@@ -1,8 +1,9 @@
 package pl.dream.dkit.data.kit;
 
+import org.bukkit.entity.Player;
 import pl.dream.dkit.data.LocalPlayer;
 
 public interface IKit {
     void getKit(LocalPlayer localPlayer);
-    void giveKit(LocalPlayer localPlayer);
+    void giveKit(Player Player);
 }
